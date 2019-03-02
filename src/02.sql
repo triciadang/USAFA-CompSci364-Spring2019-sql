8d8889408f9de9f2aee3fc9d5be9bd6057321363
dang_tricia <c20tricia.dang@usafa.edu>
2019-03-02 09:49:45 -0700

    done?

--- src/02.sql
+++ src/02.sql
@@ -1,2 +1,6 @@
 -- Having created the table structure in the prior problem, write the SQL code
 -- to enter the first two rows for the following table. (Problem 7.2)
+
+INSERT INTO EMP_1(EMP_NUM, EMP_LNAME, EMP_FNAME, EMP_INITIAL, EMP_HIREDATE, JOB_CODE)
+VALUES (101, 'News', 'John', 'G', '2000-11-08', 502), 
+	   (102, 'Senior', 'David', 'H', '1989-07-12', 501);
