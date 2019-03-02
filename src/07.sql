1cffe1a6f4864c9346ce07d03177f94317bd061a
dang_tricia <c20tricia.dang@usafa.edu>
2019-03-02 10:24:55 -0700

    07 done

--- src/07.sql
+++ src/07.sql
@@ -1,3 +1,10 @@
 -- Write a query to display the first name, last name, and email address of
 -- employees hired from January 1, 2003, to December 31, 2012. Sort the output
 -- by last name and then by first name. (Problem 7.46)
+
+SELECT EMP_FNAME, EMP_LNAME, EMP_EMAIL
+FROM LGEMPLOYEE
+WHERE EMP_HIREDATE >= '2003-01-01'
+AND EMP_HIREDATE <= '2012-12-31'
+ORDER BY EMP_LNAME ASC, EMP_FNAME;
+
