b241bac42be7d2647b1aa463d6f5ed9b3e6639ce
dang_tricia <c20tricia.dang@usafa.edu>
2019-03-02 09:58:24 -0700

    03 done

--- src/03.sql
+++ src/03.sql
@@ -1,3 +1,8 @@
 -- Assuming that the data shown in the EMP_1 table have been entered (see prior
 -- table), write the SQL code that will list all attributes for a job code of
 -- 502. Sort the results by employee number in ascending order. (Problem 7.3)
+
+SELECT *
+FROM EMP_1
+WHERE JOB_CODE = 502
+ORDER BY EMP_NUM ASC;
