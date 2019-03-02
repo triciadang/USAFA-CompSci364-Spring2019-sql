7241eb1e7a1b9ddef903d839e6abc9d6f6df94b3
dang_tricia <c20tricia.dang@usafa.edu>
2019-03-02 10:56:06 -0700

    08 done

--- src/08.sql
+++ src/08.sql
@@ -2,3 +2,8 @@
 -- department number of employees who work in department 300 or have the title
 -- "CLERK I." Sort the output by last name and then by first name. (Problem
 -- 7.47)
+
+SELECT EMP_FNAME, EMP_LNAME, EMP_PHONE, EMP_TITLE, DEPT_NUM
+FROM LGEMPLOYEE
+WHERE DEPT_NUM = 300
+OR EMP_TITLE = '%CLerk I%';
