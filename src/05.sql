18b95c505787c6f8279e6e52b5bb3f1aaba44c95
dang_tricia <c20tricia.dang@usafa.edu>
2019-03-02 10:12:40 -0700

    05 done

--- src/05.sql
+++ src/05.sql
@@ -1,3 +1,9 @@
 -- Write the SQL code to delete the row for William Smithfield, who was hired
 -- on June 22, 2004, and whose job code is 500. Hint: Use logical operators to
 -- include all of the information given in this problem. (Problem 7.5)
+
+DELETE FROM EMP_1
+WHERE EMP_LNAME = 'Smithfield'
+AND   EMP_FNAME = 'William'
+AND   EMP_HIREDATE = '2004-06-22'
+AND	  JOB_CODE = 500;
