ef097fdeac1de7b5b3eb071d1ea44781472b6e22
dang_tricia <c20tricia.dang@usafa.edu>
2019-03-02 10:16:41 -0700

    06 done

--- src/06.sql
+++ src/06.sql
@@ -1,3 +1,8 @@
 -- Write a query to display the SKU (stock keeping unit), description, type,
 -- base, category, and price for all products that have a PROD_BASE of water
 -- and a PROD_CATEGORY of sealer. (Problem 7.45)
+
+SELECT PROD_SKU, PROD_DESCRIPT, PROD_TYPE, PROD_BASE, PROD_CATEGORY, PROD_PRICE
+FROM LGPRODUCT
+WHERE PROD_BASE = 'water'
+AND   PROD_CATEGORY = 'sealer'
