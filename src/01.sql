36964817f097c5c017f933d8322e59d3e1b76b0c
dang_tricia <c20tricia.dang@usafa.edu>
2019-03-02 09:40:07 -0700

    tried to make table without subset part

--- src/01.sql
+++ src/01.sql
@@ -1,3 +1,14 @@
 -- Write the SQL code that will create the table structure for a table named
 -- EMP_1. This table is a subset of the EMPLOYEE table. Note that JOB_CODE is a
 -- foreign key to JOB. (Problem 7.1)
+
+CREATE TABLE EMP_1(
+	EMP_NUM	INTEGER,
+	EMP_LNAME	VARCHAR(15),
+	EMP_FNAME	VARCHAR(15),
+	EMP_INITIAL	CHAR(1),
+	EMP_HIREDATE	DATE,
+	JOB_CODE	INTEGER,
+	FOREIGN KEY (JOB_CODE) REFERENCES JOB(JOB_CODE)
+	ON UPDATE CASCADE ON DELETE RESTRICT
+	)
