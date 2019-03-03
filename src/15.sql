d0c1945e3cdfead0b4a685490bb475d86d233803
dang_tricia <c20tricia.dang@usafa.edu>
2019-03-03 12:06:32 -0700

    15 done - trial 1

--- src/15.sql
+++ src/15.sql
@@ -1,3 +1,8 @@
 -- Write a query to display the total inventory -- that is, the sum of all
 -- products on hand for each brand ID. Sort the output by brand ID in
 -- descending order. (Problem 7.54)
+
+SELECT BRAND_ID, SUM(PROD_QOH)
+FROM LGPRODUCT
+GROUP BY BRAND_ID
+ORDER BY BRAND_ID DESC;
