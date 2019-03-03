7c36ccbd8374da35b6838c42d47898e12bdd1ec9
dang_tricia <c20tricia.dang@usafa.edu>
2019-03-03 11:55:42 -0700

    13 done - trial 1

--- src/13.sql
+++ src/13.sql
@@ -1,2 +1,8 @@
 -- Write a query to display the number of products in each category that have a
 -- water base. Sort the results by product category. (Problem 7.52)
+
+SELECT PROD_CATEGORY, COUNT(PROD_SKU)
+FROM LGPRODUCT
+WHERE PROD_BASE = 'Water'
+GROUP BY PROD_CATEGORY
+ORDER BY PROD_CATEGORY ASC;
