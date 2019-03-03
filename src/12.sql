e13cbb67ea3ba3a535dc21d8b8baa25d64f56e20
dang_tricia <c20tricia.dang@usafa.edu>
2019-03-03 11:42:58 -0700

    12 done - 1- 11 stillnot working

--- src/12.sql
+++ src/12.sql
@@ -1,3 +1,7 @@
 -- Write a query to display a brand name and the number of products of that
 -- brand that are in the database. Sort the output by the brand name. (Problem
 -- 7.51)
+
+SELECT LGBRAND.BRAND_NAME, COUNT(BRAND_ID)
+FROM LGBRAND
+GROUP BY LGBRAND.BRAND_NAME;
