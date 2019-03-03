0bd043a4cf7217f7b83d47d19333670e6536c9cc
dang_tricia <c20tricia.dang@usafa.edu>
2019-03-03 12:00:15 -0700

    14 done - trial 1

--- src/14.sql
+++ src/14.sql
@@ -1,2 +1,7 @@
 -- Write a query to display the number of products within each base and type
 -- combination. Sort the results by product base and type. (Problem 7.53)
+
+SELECT PROD_BASE, PROD_TYPE, COUNT(PROD_SKU)
+FROM LGPRODUCT
+GROUP BY PROD_BASE, PROD_TYPE
+ORDER BY PROD_BASE ASC, PROD_TYPE;
