d557676a74ab86f6c75148ceb1b3b0e32cd279b2
dang_tricia <c20tricia.dang@usafa.edu>
2019-03-03 12:15:10 -0700

    17 done - trial 1, 16 still wrong

--- src/17.sql
+++ src/17.sql
@@ -2,3 +2,10 @@
 -- of all invoice totals for customers with cumulative invoice totals greater
 -- than $1,500. Sort the output by the sum of invoice totals in descending
 -- order. (Problem 7.58)
+
+SELECT LGCUSTOMER.CUST_CODE, LGCUSTOMER.CUST_FNAME, LGCUSTOMER.CUST_LNAME, SUM(LGINVOICE.INV_TOTAL)
+FROM LGCUSTOMER, LGINVOICE
+WHERE LGCUSTOMER.CUST_CODE = LGINVOICE.CUST_CODE
+GROUP BY LGCUSTOMER.CUST_CODE
+HAVING SUM(LGINVOICE.INV_TOTAL) > 1500
+ORDER BY SUM(LGINVOICE.INV_TOTAL) DESC;
