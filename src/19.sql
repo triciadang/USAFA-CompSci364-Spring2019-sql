a158eee77d994a1576dc7c1c7ab145f964dcc582
dang_tricia <c20tricia.dang@usafa.edu>
2019-03-03 18:50:02 -0700

    19 done

--- src/19.sql
+++ src/19.sql
@@ -1,3 +1,11 @@
 -- Write a query to display the brand ID, brand name, brand type, and average
 -- price of products for the brand that has the largest average product price.
 -- (Problem 7.63)
+
+
+SELECT LGPRODUCT.BRAND_ID, LGBRAND.BRAND_NAME, LGBRAND.BRAND_TYPE, AVG(LGPRODUCT.PROD_PRICE) AS avgprodprice
+FROM LGPRODUCT, LGBRAND
+WHERE LGBRAND.BRAND_ID = LGPRODUCT.BRAND_ID
+GROUP BY LGPRODUCT.BRAND_ID, LGBRAND.BRAND_NAME, LGBRAND.BRAND_TYPE
+ORDER BY AVG(LGPRODUCT.PROD_PRICE) DESC
+LIMIT 1;
