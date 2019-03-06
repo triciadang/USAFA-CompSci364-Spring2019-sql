57aaf65be6f6ceccfb79bec1f0e8876ccb63ad25
dang_tricia <c20tricia.dang@usafa.edu>
2019-03-06 02:36:47 -0700

    25 done - 23 AND 24 NOT RENAMED BUT DONE

--- src/25.sql
+++ src/25.sql
@@ -3,3 +3,14 @@
 -- product description, and price of any products that are not a premium brand,
 -- but that cost more than the most expensive premium brand products. (Problem
 -- 8.27)
+
+
+
+SELECT LGBRAND.BRAND_NAME, LGBRAND.BRAND_TYPE, LGPRODUCT.PROD_SKU, LGPRODUCT.PROD_DESCRIPT, 
+LGPRODUCT.PROD_PRICE
+FROM LGBRAND, LGPRODUCT
+WHERE LGBRAND.BRAND_ID = LGPRODUCT.BRAND_ID
+AND LGPRODUCT.PROD_PRICE > (SELECT MAX(LGPRODUCT.PROD_PRICE)
+						   	FROM LGPRODUCT, LGBRAND
+						   	WHERE LGBRAND.BRAND_ID = LGPRODUCT.BRAND_ID
+							AND LGBRAND.BRAND_TYPE = 'PREMIUM');
